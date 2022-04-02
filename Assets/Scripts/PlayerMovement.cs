dfec6adea26c8183ffc2b6cc6a2da4f168977b51
Waterpolo7 <31859908+Waterpolo7@users.noreply.github.com>
2022-04-02 16:26:26 +0200

    Dynamic viewer
    You can now walk and look around while using the dynamic viewer. The viewer works by holding rmb for rotation, scroll wheel for distance to your face and lmb for leaving the viewer. In Mikks scene you can find a red box, which can be interacted with using e.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    private float velocityNew;
+    private Vector3 previous;
+
+    public bool playerIsNowMoving;
+
+    Vector3 velocity;
+    public bool isGrounded;
+    // Update is called once per frame
+    void Update()
+    {
+            checkIfPlayerMoving();
+            Jumping();
+
+            //checking if player is grounded
+            isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+            if (isGrounded && velocity.y < 0)
+            {
+                velocity.y = -2f;
+            }
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+
+
+            Vector3 move = transform.right * x + transform.forward * z;
+
+            controller.Move(move * speed * Time.deltaTime);
+
+            velocity.y += gravity * Time.deltaTime;
+
+            controller.Move(velocity * Time.deltaTime);
+    }
+    
+
+    private void checkIfPlayerMoving()
+    {
+        velocityNew = ((transform.position - previous).magnitude) / Time.deltaTime;
+        previous = transform.position;
+        if (velocityNew > .1)
+        {
+            playerIsNowMoving = true;
+        }
+        if (velocityNew < .1)
+        {
+            playerIsNowMoving = false;
+        }
+    }
+
+    private void Jumping()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            //commented out jumping, coz probably wont have jumping in this game
+            //velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+    }
+}
---
+++ Assets/Scripts/crosshair.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class crosshair : MonoBehaviour
+{
+    public float interactionDistance = 4;
+    public Camera playerCamera;
+
+    public Image crosshairPanel;
+    public Color32 crosshairColor;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        crosshairPanel.color = crosshairColor;
+        RaycastHit hit;
+        if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, interactionDistance))
+        {
+            if (hit.collider.tag == "interactable")
+            {
+                crosshairColor.a = 255;
+                if (!GetComponent<ObjViewer>().isViewing)
+                {
+                    if (Input.GetKeyDown("e"))
+                    {
+                        GetComponent<ObjViewer>().ViewObject(hit.collider.gameObject);
+                        hit.collider.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+                    }
+                }
+            }
+            else
+            {
+                crosshairColor.a = 80;
+            }
+        }
+        else
+        {
+            crosshairColor.a = 80;
+        }
+    }
+}
