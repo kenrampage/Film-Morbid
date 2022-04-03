fe70745ff661f4722cfa4f68a9f8dbcf01e3a1e5
Waterpolo7 <31859908+Waterpolo7@users.noreply.github.com>
2022-04-03 23:37:16 +0200

    Remi's edits
    This is the final scene with all puzzles I made. Mikks puzzles should be added by Mikk, as he can set them up correctly.
    -NOTE: The clock puzzle has some rotation problems when the base object is rotated, so please keep the CLOCKSAFE objects rotation to (0, 0, 0).

--- Assets/MusicPuzzle.cs
+++ Assets/MusicPuzzle.cs
@@ -11,6 +11,8 @@ public class MusicPuzzle : MonoBehaviour
 
     [SerializeField] MusicDot[] musicDots;
 
+    bool won;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,44 +22,49 @@ public class MusicPuzzle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (checking)
+        if (!won)
         {
-            timer += Time.deltaTime;
-            //Basically animations
-            if (!beenRight) {
-                if (timer < 0.6f)
-                {
-                    trebClef.transform.Rotate(transform.forward * Time.deltaTime * 50f);
-                }
-                else
-                {
-                    beenRight = true;
-                }
-            }
-            else if (beenRight)
+            if (checking)
             {
-                if(timer < 1.2f)
+                timer += Time.deltaTime;
+                //Basically animations
+                if (!beenRight)
                 {
-                    trebClef.transform.Rotate(-transform.forward * Time.deltaTime * 50f);
-                }
-                else
-                {
-                    if(musicDots[0].currentPt == 5 && musicDots[1].currentPt == 5 && musicDots[2].currentPt == 6 && musicDots[3].currentPt == 4 && musicDots[4].currentPt == 5 && musicDots[5].currentPt == 6)
+                    if (timer < 0.6f)
                     {
-                        trebClef.gameObject.AddComponent<Rigidbody>();
-                        trebClef.gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * 2, ForceMode.Impulse);
-                        trebClef.gameObject.tag = "holdable";
-                        for (int i = 0; i < 6; i++)
-                        {
-                            Rigidbody rb = musicDots[i].gameObject.AddComponent<Rigidbody>();
-                            rb.AddForce(transform.forward * 2, ForceMode.Impulse);
-                            musicDots[i].gameObject.tag = "holdable";
-                        }
-                        Debug.Log("Won Music");
+                        trebClef.transform.Rotate(transform.forward * Time.deltaTime * 50f);
                     }
-                    beenRight = false;
-                    timer = 0f;
-                    checking = false;
+                    else
+                    {
+                        beenRight = true;
+                    }
+                }
+                else if (beenRight)
+                {
+                    if (timer < 1.2f)
+                    {
+                        trebClef.transform.Rotate(-transform.forward * Time.deltaTime * 50f);
+                    }
+                    else
+                    {
+                        if (musicDots[0].currentPt == 5 && musicDots[1].currentPt == 5 && musicDots[2].currentPt == 6 && musicDots[3].currentPt == 4 && musicDots[4].currentPt == 5 && musicDots[5].currentPt == 6)
+                        {
+                            trebClef.gameObject.AddComponent<Rigidbody>();
+                            trebClef.gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * 2, ForceMode.Impulse);
+                            trebClef.gameObject.tag = "holdable";
+                            for (int i = 0; i < 6; i++)
+                            {
+                                Rigidbody rb = musicDots[i].gameObject.AddComponent<Rigidbody>();
+                                rb.AddForce(transform.forward * 2, ForceMode.Impulse);
+                                musicDots[i].gameObject.tag = "holdable";
+                            }
+                            Debug.Log("Won Music");
+                        }
+                        won = true;
+                        beenRight = false;
+                        timer = 0f;
+                        checking = false;
+                    }
                 }
             }
         }
