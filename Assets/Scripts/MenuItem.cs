fef67febea11bcb5633db76c12d2f2a5f7cb63b8
Waterpolo7 <31859908+Waterpolo7@users.noreply.github.com>
2022-04-11 19:56:59 +0200

    menu fix?

--- Assets/Scripts/MenuItem.cs
+++ Assets/Scripts/MenuItem.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class MenuItem : MonoBehaviour
 {
     [SerializeField] float xOffset, yOffset;
-    private Vector3 origPos;
+    public Vector3 origPos;
     // Start is called before the first frame update
     void Start()
     {
-        origPos = transform.position;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(origPos.x - (Input.mousePosition.x - Screen.width / 2) * xOffset, origPos.y - (Input.mousePosition.y - Screen.height / 2) * yOffset, 0);
+        transform.localPosition = new Vector3(origPos.x - (Input.mousePosition.x - Screen.width / 2) * xOffset, origPos.y - (Input.mousePosition.y - Screen.height / 2) * yOffset, 0);
     }
 }
